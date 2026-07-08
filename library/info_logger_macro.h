851d1727f55bfa467c06606694199227f28bb987
JoanA928 <joanaponte928@gmail.com>
2026-07-08 13:49:45 -0700

    wip: add debug macros to personal library

---
+++ library/info_logger_macro.h
@@ -0,0 +1,27 @@
+/***********************************************************************
+ * Class:: N/A
+ * Name::  Joan Aponte
+ * Student ID:: 924074746
+ * Github-Name:: JoanA928
+ * Project:: Firmware - Data Logger
+ * File:: info_logger_macrco.h
+ *
+ * Description:: This file makes it easy to write consistent, and informative
+ * debug, error, info messages to the approrpiate file stream.
+ *
+ **********************************************************************/
+
+#ifndef INFO_LOGGER_MACRO_H
+#define INFO_LOGGER_MACRO_H
+
+#include <stdio.h>
+
+#define LOG_DEBUG(fmt, ...)                                                    \
+  fprintf(stderr, "[DEBUG] %s:%d: " fmt "\n", __FILE__, __LINE__,              \
+          ##__VA_ARGS__) // fmt -> first macro parameter
+#define LOG_INFO(fmt, ...) fprintf(stderr, "[INFO] " fmt "\n", ##__VA_ARGS__)
+
+#define LOG_ERROR(fmt, ...)                                                    \
+  fprintf(stderr, "[ERROR] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+#endif // !INFO_LOGGER_MACRO_H
